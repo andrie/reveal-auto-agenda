e8536f9919b48dd12ed4c4ca456f621b2ba759d9
andrie <apdevries@gmail.com>
2022-08-03 23:28:00 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# reveal-agenda
+
+A quarto extension for `reveal.js` that automatically creates agenda slides from H1 heading titles.
+
+## Installation
+
+To install this extension in your current directory (or into the Quarto project that you're currently working in),  use the following command:
+
+```
+quarto install extension andrie/reveal-agenda
+```
+
+## Enabling
+
+Add this to your document or project options:
+
+```yaml
+filters:
+  - reveal-agenda
+```
+
+## Usage
+
+To be completed...
+
+
+![](https://user-images.githubusercontent.com/104391/178159800-e3b74510-97e7-469c-87cf-b49cac8602f9.png)
+
+Non-HTML formats will still have the filename, but it will simply be shown in bold above the code block.
+
+You can see the published version of this example at <https://quarto-ext.github.io/reveal-agenda/> ([Source code](example.qmd)).
+
---
+++ _extensions/reveal-agenda/reveal-agenda.lua
@@ -0,0 +1,56 @@
+
+
+-- for code blocks w/ filename create an enclosing div:
+-- <div class="code-with-filename">
+--   <div class="code-with-filename-file">
+--     <pre>filename.py</pre>
+--   </div>
+--   <div class="sourceCode" id="cb1" data-filename="filename.py">
+--     <pre></pre>
+--   </div>
+-- </div>
+
+
+local text = pandoc.text
+
+local headers = pandoc.List()
+
+function scan_headers(el)
+  if el.level == 1 then
+    local htext = el:walk {
+      Str = function(el)
+        headers:insert(el.text)
+      end
+    }
+  end
+end
+
+
+local newBlocks = pandoc.List()
+
+function scan_blocks(blocks)
+  
+  for _,block in ipairs(blocks) do
+    newBlocks:insert(block)
+    if (block.t == "Header" and block.level == 1) then
+      newBlocks:insert(pandoc.BulletList(headers))
+    end
+  end
+  
+  -- if we found a file name then return the modified list
+  -- of blocks and ensure our dependency w/ css is injected
+  -- if foundFilename then
+  --   quarto.doc.addHtmlDependency({
+  --     name = "reveal-agenda",
+  --     version = "0.0.1",
+  --     stylesheets = {"reveal-agenda.css"}
+  --   })
+  --   return newBlocks
+  -- else
+  --   return blocks
+  -- end
+  return newBlocks
+end
+
+
+return {{Header = scan_headers}, {Blocks = scan_blocks}}
