3634c86496a93b9f07a165411aec28f3e55b712a
andrie <apdevries@gmail.com>
2022-08-04 15:28:21 -0400

    Add CSS; hide slide title on agenda slides

--- _extensions/reveal-agenda/reveal-agenda.lua
+++ _extensions/reveal-agenda/reveal-agenda.lua
@@ -15,42 +15,78 @@ local text = pandoc.text
 
 local headers = pandoc.List()
 
+function get_header_text(el)
+  local z = ""
+  local htext = el:walk {
+    Str = function(el)
+      z = el.text
+    end
+  }
+  return z
+end
+
 function scan_headers(el)
   if el.level == 1 then
-    local htext = el:walk {
-      Str = function(el)
-        headers:insert(el.text)
-      end
-    }
+    headers:insert(get_header_text(el))
   end
 end
 
 
 local newBlocks = pandoc.List()
 
+
+function change_header_class(el)
+  el.attr = pandoc.Attr("", {"agenda-slide"})
+  quarto.utils.dump(el.attributes)
+  return el
+end
+
 function scan_blocks(blocks)
   
   for _,block in ipairs(blocks) do
-    newBlocks:insert(block)
     if (block.t == "Header" and block.level == 1) then
-      newBlocks:insert(pandoc.BulletList(headers))
+      change_header_class(block)
+      newBlocks:insert(block)
+
+      -- insert the original header
+      -- newBlocks:insert(
+      --   pandoc.Header(
+      --     1, block.text, pandoc.Attr("", {"agenda-slide"})
+      --   )
+      -- )
+
+      -- modify the agenda items for active agenda item
+      local mod_headers = pandoc.List()
+      for i=1, #headers do 
+        if (headers[i] == get_header_text(block)) then
+          mod_headers:insert(
+            pandoc.Div(headers[i], pandoc.Attr("", {"agenda-active"}))
+          )
+        else
+          mod_headers:insert(headers[i])
+        end
+      end
+
+      -- insert the agenda items
+      newBlocks:insert(
+        pandoc.Div(
+          pandoc.BulletList(mod_headers),
+          pandoc.Attr("", {"agenda"})
+        )  
+      )
+    else
+      newBlocks:insert(block)
     end
   end
   
-  -- if we found a file name then return the modified list
-  -- of blocks and ensure our dependency w/ css is injected
-  -- if foundFilename then
-  --   quarto.doc.addHtmlDependency({
-  --     name = "reveal-agenda",
-  --     version = "0.0.1",
-  --     stylesheets = {"reveal-agenda.css"}
-  --   })
-  --   return newBlocks
-  -- else
-  --   return blocks
-  -- end
+  -- inject the CSS dependency
+  quarto.doc.addHtmlDependency({
+    name = "reveal-agenda",
+    version = "0.0.1",
+    stylesheets = {"reveal-agenda.css"}
+  })
+
   return newBlocks
 end
-
 
 return {{Header = scan_headers}, {Blocks = scan_blocks}}
